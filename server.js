1d1f92ea918886033f339301a9b28fc302a11f3e
Glitch (fluff-channel) <none>
2018-05-05 10:00:37 -0400

    mongodb and url

--- server.js
+++ server.js
@@ -1,32 +1,128 @@
 
 'use strict';
 
-var fs = require('fs');
-var mongoose = require('mongoose');
-var express = require('express');
-var app = express();
+let fs = require('fs');
+let mongoose = require('mongoose');
+let express = require('express');
+let app = express();
+let promise;
+let connectionString = 'mongodb://jaknap_9:2tunacan@ds259079.mlab.com:59079/url_hash'; 
+let bodyParser = require('body-parser');
+let btoa = require('btoa');
+let atob = require('atob');
+
+//mongoose.connect('mongodb://jaknap_9:2tunacan@ds259079.mlab.com:59079/url_hash',{useMongoClient: true});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
-mongoose.connect('mongodb://jaknap_9:xxxxx@ds259079.mlab.com:59079/url_hash',{useMongoClient: true});
+
+app.route('/')
+    .get(function(req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+    })
+
+
 
 let countersSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     count: { type: Number, default: 0 }
 });
 
+let Counter = mongoose.model('Counter', countersSchema);
 
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
+
+
+
+let urlSchema = new mongoose.Schema({
+    _id: {type: Number},
+    url: '',
+    created_at: ''
+});
+
+urlSchema.pre('save', function(next) {
+    console.log('running pre-save');
+    let doc = this;
+    Counter.findByIdAndUpdate({ _id: 'url_count' }, { $inc: { count: 1 } }, function(err, counter) {
+        if(err) return next(err);
+        console.log(counter);
+        console.log(counter.count);
+        doc._id = counter.count;
+        doc.created_at = new Date();
+        console.log(doc);
+        next();
+    });
+});
+let URL = mongoose.model('shortUrl', urlSchema);
+
+promise = mongoose.connect(connectionString);
+promise.then(function(db) {
+    console.log('connected!');
+    URL.remove({}, function() {
+        console.log('URL collection removed');
+    })
+    Counter.remove({}, function() {
+        console.log('Counter collection removed');
+        let counter = new Counter({_id: 'url_count', count: 10000});
+        counter.save(function(err) {
+            if(err) return console.error(err);
+            console.log('counter inserted');
+        });
+    });
+});
+
+
+app.get('/:hash', function(req, res) {
+let baseid = req.params.hash;
+if(baseid) {
+    let id = atob(baseid);
+    console.log('db id' + id);
+    URL.findOne({ _id: id }, function(err, doc) {
+        if(doc) {
+            res.redirect(doc.url);
+        } else {
+            res.redirect('/');
+        }
+    });
 }
+});
+
+
+app.get('/new*', function(req, res, next) {
+    let urlData = (req.path).slice(5);
+      //{ "original_url":"http://foo.com:80", "short_url":"https://little-url.herokuapp.com/8170" }
+    URL.findOne({url: urlData}, function(err, doc) {
+        if(doc) {
+            console.log('Found record');
+            res.send({
+                "original_url": urlData,
+                "shortened_url": "https://fluff-channel.glitch.me/" + btoa(doc._id)
+            });
+        } else {
+            console.log('Create new document');
+            let url = new URL({
+                url: urlData
+            });
+            url.save(function(err) {
+                if(err) {
+                    return console.error(err);
+                }
+                console.log(urlData);
+                res.send({
+                    "original_url": urlData,
+                    "short_url": "https://fluff-channel.glitch.me/" + btoa(url._id)
+                });
+            });
+        }
+    });
+});
+
+
+      
+
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -39,21 +135,6 @@ app.route('/_api/package.json')
     });
   });
 
-/*
-app.route('/')
-    .get(function(req, res) {
-      res.sendFile(process.cwd() + '/views/index.html');
-    })
-*/
-
-app.route('/new*')
-    .get(function(req, res) {
-      //console.log();
-      let returnVal = (req.path).slice(5);
-      //{ "original_url":"http://foo.com:80", "short_url":"https://little-url.herokuapp.com/8170" }
-      let jsonBody = {"original_url": returnVal};
-      res.send(jsonBody);
-    })
 
 
 // Respond not found to all the wrong routes
